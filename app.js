ff96acc0c2d958460da3dbc8c103831dedb114e6
mtotodev05 <mtotodev05@gmail.com>
2018-07-21 22:16:25 +0300

    updated user models

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var expressHandleBars = require('express-handlebars');
-
 var app = express();
+var mongoose = require('mongoose');
+var session = require('express-session');
+
+//mongoose.connect('mongodb://kamau:wamatu@localhost:27017/Shopping')
+mongoose.connect('mongodb://localhost:27017/ShopCart',{useNewUrlParser:true})
 
 // view engine setup
 app.engine('.hbs',expressHandleBars({defaultLayout:'layout',extname:'.hbs'}));
@@ -18,6 +21,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: 'mysupersecret',resave:false,saveUninitialized:false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -33,10 +37,8 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
 module.exports = app;
