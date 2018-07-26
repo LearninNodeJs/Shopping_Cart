a653ca7d1ad1ac7f0ebdbbefd09c1abc16c25381
mtotodev05 <mtotodev05@gmail.com>
2018-07-26 22:49:12 +0300

    fixed bug in controller

--- controller/util.js
+++ controller/util.js
@@ -1,3 +1,8 @@
+var Cart = require('../models/cart');
 exports.showIndex = function (req,res,next){
-    res.render('shop/checkout');
-}
+    if(!req.session.cart){
+        return res.redirect('/shop-cart');
+    }
+    var cart = new Cart(req.session.cart);
+    res.render('shop/checkout',{total: cart.totalPrice});
+};
