af830745b8c5a276c1b48dcdf284fec8be09778d
mtotodev05 <mtotodev05@gmail.com>
2018-07-30 06:42:11 +0300

    Fixed Bug in remove Items in Cart Price Handler

--- models/cart.js
+++ models/cart.js
@@ -24,7 +24,7 @@ module.exports = function Cart(initItems){
    };
    this.removeItems = function(id){
      this.totalQuantity -=this.items[id].quantity;
-     this.totalPrice -=this.items[id].item.price;
+     this.totalPrice -=this.items[id].price;
      delete this.items[id];
    };
    this.generateItemsArray = function(){
